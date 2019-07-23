152715eaa7e41fee6ae01240ac4581a89d142731
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-07-22 21:04:39 -0500

    add sirenix and arachnid tools

---
+++ Language Quest/Assets/Arachnid/Data.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arachnid
+{
+	public enum PropertyType { Local, Global }
+	public enum TargetType { Nearest, Farthest, Random}
+}
---
+++ Language Quest/Assets/Arachnid/Events/EventTrigger2D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.Events;
+
+namespace Arachnid {
+
+    [RequireComponent(typeof(Collider2D))]
+    [TypeInfoBox("When the attached 2D Collider is triggered, will Invoke the events contained below.")]
+    public class EventTrigger2D : FilteredTrigger2D
+    {
+        [AssetsOnly]
+        public List<GameEvent> events = new List<GameEvent>();
+        public UnityEvent uEvent;
+
+        protected override void OnTriggered(Collider2D triggerer)
+        {
+            uEvent.Invoke();
+            foreach (var e in events) e.Raise();
+        }
+    }
+}
