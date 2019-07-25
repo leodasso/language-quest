3ccc15f0108639b76acd5911d917cc5ef2d6bb66
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-07-24 23:12:13 -0500

    Create and apply some props

--- Language Quest/Assets/Scripts/Prop.cs
+++ Language Quest/Assets/Scripts/Prop.cs
@@ -7,16 +7,36 @@ public class Prop : MonoBehaviour
 {
     [AssetsOnly]
     public PropDef propDef;
+    SpriteRenderer _spriteRenderer;
+    Collider2D _collider;
+    readonly Color _lockedColor = new Color(0, 0, 0, .1f);
     
     // Start is called before the first frame update
     void Start()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<Collider2D>();
         
+        if (propDef)
+            SetUnlocked(propDef.unlocked);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetUnlocked(bool isUnlocked)
+    {
+        if (_spriteRenderer)
+        {
+            _spriteRenderer.color = isUnlocked ? Color.white : _lockedColor;
+        }
+
+        if (_collider)
+        {
+            _collider.enabled = isUnlocked;
+        }
     }
 }
