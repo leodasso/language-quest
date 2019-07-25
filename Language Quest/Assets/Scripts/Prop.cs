801f2b2c85dcc8c675d6e4c998f44de17107791e
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-07-24 22:49:38 -0500

    Start on kitchen scene, add scripts for prop and language

---
+++ Language Quest/Assets/Scripts/Prop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Prop : MonoBehaviour
+{
+    [AssetsOnly]
+    public PropDef propDef;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
